de8b5673cf08ba12adb4fffa2c455a7269c1189f
yc <yckyos261@gmail.com>
2014-11-11 11:43:28 +0800

    简单编辑器

---
+++ 简单程序编辑器/简单程序编辑器/FrmChild.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace 简单程序编辑器
+{
+    public partial class FrmChild : Form
+    {
+        public RichTextBox ContentText
+        {
+            get
+            {
+                return this.richTextBox1;
+            }
+        }
+
+        public ArrayList Keywords;
+
+        public FrmChild()
+        {
+            InitializeComponent();
+        }
+
+        public void StopTextChanged()
+        {
+            this.richTextBox1.TextChanged -= new System.EventHandler(
+                this.richTextBox1_TextChanged);
+        }
+
+        public void StartTextChanged()
+        {
+            this.richTextBox1.TextChanged += new System.EventHandler(
+                this.richTextBox1_TextChanged);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(this.richTextBox1.SelectedText))
+            {
+                return;
+            }
+
+            this.richTextBox1.SelectionColor = Color.Black;//默认颜色-黑色
+            int index = this.richTextBox1.SelectionStart;   //当前选中文本位置
+
+            string strline = this.richTextBox1.Lines[       //当前行
+                this.richTextBox1.GetLineFromCharIndex(
+                this.richTextBox1.SelectionStart)];
+            Console.WriteLine(strline);
+            string[] wordList = strline.Trim().Split(' ', ';', '.', '{', '}', '(', ')');
+            if (wordList.Length == 0)
+            {
+                return;
+            }
+
+            foreach (string word in wordList)
+            {
+                if (this.Keywords.Contains(word))   //处理关键字变色
+                {
+                    this.richTextBox1.Select(index - word.Length, word.Length);
+                    this.richTextBox1.SelectionColor = Color.Blue;
+
+                    this.richTextBox1.Select(index, 0);
+                    this.richTextBox1.SelectionColor = Color.Black;
+                }
+                //
+                else if (word.StartsWith("//"))      //处理行尾注释
+                {
+                    this.richTextBox1.Select(index-2, strline.Length - index+2);
+                    this.richTextBox1.SelectionColor = Color.Green;
+
+                    this.richTextBox1.Select(index, 0);
+                    break;
+                }
+                else   //非关键字
+                {
+                    this.richTextBox1.Select(index - word.Length, word.Length);
+                    this.richTextBox1.SelectionColor = Color.Black;
+
+                    this.richTextBox1.Select(index, 0);
+                    this.richTextBox1.SelectionColor = Color.Black;
+                }
+            }
+        }
+    }
+}
---
+++ 简单程序编辑器/简单程序编辑器/FrmMain.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace 简单程序编辑器
+{
+    public partial class FrmMain : Form
+    {
+        ArrayList Keywords;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            Keywords = new ArrayList();
+            Keywords.Add("using");
+            Keywords.Add("System");
+            Keywords.Add("public");
+            Keywords.Add("private");
+            Keywords.Add("void");
+            Keywords.Add("int");
+            Keywords.Add("char");
+            Keywords.Add("string");
+            Keywords.Add("return");
+        }
+
+        /// <summary>
+        /// 新建
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShowNewForm(object sender, EventArgs e)
+        {
+            FrmChild child = new FrmChild();
+            child.MdiParent = this;
+            child.Text = "Class" + this.MdiChildren.Length + ".cpp";
+            child.Keywords = this.Keywords;
+            child.Show();
+            child.FormClosed += new FormClosedEventHandler(child_FormClosed);
+
+            TreeNode node = new TreeNode();
+            node.Text = child.Text;
+            this.tvSolution.Nodes.Add(node);
+        }
+
+        /// <summary>
+        /// 子窗体关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void child_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.FormOwnerClosing)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)sender;
+            foreach (TreeNode node in tvSolution.Nodes)
+            {
+                if (node.Text == child.Text)
+                {
+                    node.Remove();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 打开文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Filter = "代码文件(*.cs)|*.cs|所有文件(*.*)|*.*";
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                string FileName = openFileDialog.FileName;
+            }
+
+            FrmChild child = new FrmChild();
+            child.MdiParent = this;
+            child.Text = openFileDialog.FileName;
+            child.Keywords = this.Keywords;
+            child.Show();
+            child.ContentText.LoadFile(openFileDialog.FileName, RichTextBoxStreamType.PlainText);
+            child.FormClosed += new FormClosedEventHandler(child_FormClosed);
+        }
+
+        /// <summary>
+        /// 颜色
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmiColor_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            ColorDialog colorDlg = new ColorDialog();
+            DialogResult dr = colorDlg.ShowDialog();
+            if (DialogResult.OK != dr)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.StopTextChanged();
+            child.ContentText.SelectionColor = colorDlg.Color;
+            child.StartTextChanged();
+        }
+
+        /// <summary>
+        /// 字体
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmiFont_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            FontDialog fontDlg = new FontDialog();
+            DialogResult dr = fontDlg.ShowDialog();
+            if (DialogResult.OK != dr)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.ContentText.SelectionFont = fontDlg.Font;
+        }
+
+        #region 缩进
+        private void tsbIncreaseIndent_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.ContentText.SelectionIndent += 30;
+        }
+
+        private void tsbReduceIndent_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.ContentText.SelectionIndent -= 30;
+        }
+        #endregion
+
+        /// <summary>
+        /// 注释
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsbAnnotation_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.StopTextChanged();
+
+            int index = child.ContentText.SelectionStart;
+            int line = child.ContentText.GetLineFromCharIndex(
+                child.ContentText.SelectionStart);
+
+            child.ContentText.Text = child.ContentText.Text.Insert(
+                child.ContentText.SelectionStart, "//");
+            string strline = child.ContentText.Lines[line];
+            child.ContentText.Select(index, index + strline.Length);
+            child.ContentText.SelectionColor = Color.Green;
+
+            child.ContentText.Select(index, 0);
+            child.StartTextChanged();
+            return;
+        }
+
+        #region 窗口菜单
+        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+
+        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form childForm in MdiChildren)
+            {
+                childForm.Close();
+            }
+        }
+        #endregion
+
+        #region 编辑菜单
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.ContentText.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.ContentText.Redo();
+        }
+
+        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.ContentText.Cut();
+        }
+
+        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.ContentText.Copy();
+        }
+
+        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.ContentText.Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild == null)
+            {
+                return;
+            }
+
+            FrmChild child = (FrmChild)this.ActiveMdiChild;
+            child.ContentText.SelectAll();
+        }
+        #endregion
+    }
+}
